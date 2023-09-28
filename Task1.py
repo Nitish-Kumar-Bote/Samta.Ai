042368f7c16905759bc5c3131091423383a6d790
Nitish Bote <princekumarbote@gmail.com>
2023-09-28 07:33:53 +0530

    initial commit

---
+++ Task1.py
@@ -0,0 +1,19 @@
+def calculate_area(length, width):
+    if length == width:
+        return "This is a square!"
+    else:
+        return length * width
+
+def main():
+    length = float(input("Enter the length: "))
+    width = float(input("Enter the width: "))
+
+    result = calculate_area(length, width)
+
+    if isinstance(result, str):
+        print(result)
+    else:
+        print(f"The area of the rectangle is: {result}")
+
+if __name__ == "__main__":
+    main()
